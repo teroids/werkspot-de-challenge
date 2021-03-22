16d1f0c30a6243210c8526a2634d1f21a1c29cdd
teroids <teroids@gmail.com>
2021-03-22 12:23:20 -0600

    solve edge case

--- models/events/availability_snapshot.sql
+++ models/events/availability_snapshot.sql
@@ -1,6 +1,6 @@
 WITH calendar AS (
 SELECT
-    t.day::date AS date
+    t.day::date AS calendar_day
 FROM
 	generate_series(
 		(SELECT min(created_at) FROM {{ source('raw','event_log') }} ),
@@ -8,27 +8,37 @@ FROM
 		interval '1 day'
 	) AS t(day)
 ),
-intermediate_model AS (
+transform_data AS (
 SELECT
+    professional_id_anonymized as professional_id,
 	CASE WHEN event_type IN ('proposed', 'became_able_to_propose') THEN 1 ELSE 0 END AS is_working,
 	created_at,
 	LEAD(created_at,1,timestamp '2020-03-10') OVER (PARTITION BY professional_id_anonymized ORDER BY created_at) AS next_at
 FROM
 	{{ source('raw','event_log') }}
 ),
-final_model AS (
+intermediate_model AS (
 SELECT
-    date,
-    COALESCE(SUM(is_working),0)
+    *,
+    row_number() OVER (PARTITION BY professional_id, calendar_day ORDER BY created_at DESC) AS rn
 FROM
     calendar
-LEFT JOIN
-    intermediate_model
+JOIN
+    transform_data
 ON
-    date>=created_at and date<=next_at
+    calendar_day>=created_at AND calendar_day<=next_at
+),
+final_model AS (
+SELECT
+    calendar_day,
+    SUM(COALESCE(is_working,0))
+FROM
+    intermediate_model
+WHERE
+    rn = 1
 GROUP BY
-    date
+    calendar_day
 ORDER BY
-    date
+    calendar_day
 )
 SELECT * FROM final_model
